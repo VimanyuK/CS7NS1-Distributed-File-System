d6c7a11f030811cd184a084a9096bb1098ddc014
vimanyu <vimanyukachroo@yahoo.com>
2017-12-15 13:37:12 +0000

    File server function def.
    constructed the required structure for the File server to store clients files.

---
+++ File_server.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+#Stores the files for clients; assigned by the DirServer
+
+class FileServer():
+	# Takes and stores the client files.
+
+	def GET():
+		pass
+
+	def POST():
+		pass
+
+	def DELETE():
+		pass
+
+	def HEAD():
+		pass
+
+	pass
+_config = { 'lockserver' : None,
+	    'nameserver' : None,
+	    'directories' : [],
+	    'fsroot' : 'fs/',
+	    'server' : None
+	  }
+
