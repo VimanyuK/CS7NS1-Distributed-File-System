227b8ef50119dfd878c824cda4c33fa58dc81458
vimanyu <vimanyukachroo@yahoo.com>
2017-12-15 14:54:20 +0000

    FileServer updated, Protocol uploaded
    Protocol has functions that will be used through out DFS.

--- File_server.py
+++ File_server.py
@@ -2,22 +2,46 @@
 
 #Stores the files for clients; assigned by the DirServer
 
+import os.path
+import time
+import web
+
+
 class FileServer():
 	# Takes and stores the client files.
+    def GET(self, file_path):
+        """Return the requested file if it's not locked"""
+        p = Local_path(file_path)
+        web.header('Last-Modified', time.ctime(os.path.getmtime(p)))
+        with open(p) as f:
+            return f.read()
 
-	def GET():
-		pass
+    def POST(self,file_path):
+        p = Local_path(file_path)
+        with open(p, 'w') as f:
+            f.write(web.data())
 
-	def POST():
-		pass
+        web.header('Last-Modified', time.ctime(os.path.getmtime(p)))
+        return ''
+        
+    def DELETE(self, file_path):
+        """Remove the filepath if it's unlocked"""
+        web.header('Content-Type', 'text/plain; charset=UTF-8')
+        os.unlink(Local_path(file_path))
+        return 'OK'
+        
+    def HEAD(self, file_path):
+        """If the file exists/isn't locked, return the last-modified http
+           header which corresponds to the last time was modified."""
+        web.header('Content-Type', 'text/plain; charset=UTF-8')
+        p = Local_path(file_path)
+        web.header('Last-Modified', time.ctime(os.path.getmtime(p)))
+        return ''
 
-	def DELETE():
-		pass
 
-	def HEAD():
-		pass
+def Local_path(file_path):
+    return os.path.join(os.getcwd(), _config['fsroot'], file_path[1:])
 
-	pass
 _config = { 'lockserver' : None,
 	    'nameserver' : None,
 	    'directories' : [],
---
+++ Protocol.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+# Protocols for parsing the files and tasks carried out for every interaction between the servers
+
+import json
+import os.path
+
+def load_config(config,file_path):
+	## parse data from config files
+    if not os.path.exists(file_path):
+        return
+        with open(file_path) as f:
+            c = json.loads(f.read())
+            config.update(c)
+
